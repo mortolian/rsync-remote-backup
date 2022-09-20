1efcb6eca25ed2ce2322a035032d5affbaff8064
Gideon <gideon@mortolio.com>
2022-09-20 19:29:16 +0200

    Milestone commit for backup.

--- src/backup.py
+++ src/backup.py
@@ -1,242 +1,222 @@
-#!/usr/bin/python
-
-""" Rsync Backup Tool
-
+"""
+Rsync Backup Tool
+----------------------------------------------------------------------
 This is a very simple tool which facilitates the automated backup of
 a linux or unix source path to a local path using rsync.
-
---dry-run       Will run the rsync without making any changes to the source or destination.
---config=[]     If you require to set a specific or different config files for the program.
-                The default config filename is: config.yml
--h --help       This will display the program help.
-
-USAGE : rsync_backup.py -h --help --config=[] --dry-run
+----------------------------------------------------------------------
 LICENSE : GNU GPLv3
 """
-
+import argparse
 import os
+import socket
 import subprocess
 import time
-import re
-import sys
-import getopt
-import socket
 import yaml
+from termcolor import colored, cprint
+from dataclasses import dataclass
+
+RSYNC_VERSION = '2.6.9'
+DEFAULT_CONFIG = 'config.yaml'
 
 
-def path_check(path):
+@dataclass
+class ConfigSyncPathsDataClass:
+    name: str
+    remote_path: str
+    local_path: str
+
+
+@dataclass
+class ConfigDataClass:
+    job_name: str
+    description: str
+    remote_host: str
+    remote_user: str
+    sync_paths: list[ConfigSyncPathsDataClass]
+    rsync_options: str
+
+
+@dataclass
+class ConfigListDataClass:
+    jobs: list[ConfigDataClass]
+
+
+class RsyncNotFoundException(Exception):
+    pass
+
+
+class RemoveSocketNotFoundException(Exception):
+    pass
+
+
+class PathNotFoundException(Exception):
+    pass
+
+
+class ConfigJobNotFoundException(Exception):
+    pass
+
+
+def readConfig(config_file_path: str) -> ConfigListDataClass:
+    """
+    check if there is a config argument set and use that file else
+    use the default config file.
+    """
+    with open(config_file_path, 'r') as config_file_content:
+        try:
+            configs = yaml.safe_load(config_file_content.read())
+            return ConfigListDataClass(
+                jobs=[ConfigDataClass(
+                    job_name=c,
+                    description=configs[c]['description'],
+                    remote_host=configs[c]['remote']['host'],
+                    remote_user=configs[c]['remote']['user'],
+                    sync_paths=[ConfigSyncPathsDataClass(
+                        name=p,
+                        remote_path=configs[c]['sync']['paths'][p][
+                            'remote_path'],
+                        local_path=configs[c]['sync']['paths'][p][
+                            'local_path'],
+                    ) for p in configs[c]['sync']['paths']],
+                    rsync_options=configs[c]['rsync']['options'],
+                ) for c in configs]
+            )
+        except yaml.YAMLError as e:
+            raise e
+        except KeyError as e:
+            raise KeyError(f'The configuration file seems to have '
+                           f'an error: {e}')
+
+
+def checkRemoteSocket(host: str, port: int = 22) -> bool:
+    """
+    Do a test to see that the host is available and that the
+    required SSH port is available to connect to.
+    """
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((host, port))
+    if result != 0:
+        raise RemoveSocketNotFoundException(
+            f'Remote host {host} did not answer on '
+            f'port {port}.')
+
+    return True
+
+
+def pathCheck(path) -> bool:
     """
     Checks whether a path exists and is a folder path (dir).
-    :param path:
-    :return bool:
     """
     if os.path.exists(path) and os.path.isdir(path):
         return True
 
-    return False
+    raise PathNotFoundException(f'The path \'{path}\' could not be found.')
 
 
-def ping_host(host):
+def rsyncCheck(version: str) -> bool:
     """
-    This method pings the host to check if it receives a response and if the host is up.
-
-    We send 5 packets ('-c 5') and wait 3 milliseconds ('-W 3') for a response. The function returns
-    the return code from the ping utility.
-
-    0 - Success
-    1 - No Reply
-    2 - Other Errors
-
-    :param host:
-    :return numerical:
+    This will check if rsync exists and if it matches the
+    specified version. This can be useful to check what
+    command arrangement will work.
     """
-    ret_code = subprocess.call(['ping', '-c', '5', '-W', '3', host], stdout=open(os.devnull, 'w'), stderr=open(os.devnull, 'w'))
-    return ret_code
-
-
-def host_socket_test(host, port):
-    """
-    Test a host by connecting to a socket on the host.
-    :param host:
-    :param port:
-    :return bool:
-    """
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex((host, port))
-    if result == 0:
-        return True
-
-    return False
-
-
-def generate_rsync_cmd(user, host, options, src_paths, dst_path, dry_run=False):
-    """
-    This will generate the rsync command which will make the backup.
-
-    :param user:
-    :param host:
-    :param options:
-    :param src_paths:
-    :param dst_path:
-    :param dry_run:
-    :return mixed:
-    """
-    if len(src_paths) > 0 and isinstance(src_paths, list):
-        path_list = "'" + ' '.join(src_paths) + "'"
-    else:
-        return False
-
-    if not user:
-        return False
-
-    if not host:
-        return False
-
-    if not options:
-        return False
-
-    if dry_run:
-        options = options + " --dry-run"
-
-    if not dst_path:
-        return False
-
-    the_command = "rsync {attribs} {user}@{host}:{src_paths} {dst_drive_path}".format(
-        attribs=options,
-        user=user,
-        host=host,
-        src_paths=path_list,
-        dst_drive_path=dst_path
+    response = subprocess.run(
+        ['rsync', '--version'],
+        capture_output=True,
+        text=True
     )
 
-    return the_command
+    if response.stdout.find(version) == -1 or response.returncode != 0:
+        raise RsyncNotFoundException(f'RSYNC is missing or not the '
+                                     f'correct version. RSYNC {RSYNC_VERSION} '
+                                     f'needed.')
+
+    return True
 
 
-def rsync_check(version):
+def compileRsyncCommand(config: ConfigDataClass) -> str:
+    """ This creates the rsync command and validates all the information. """
+    return ''
+
+
+def rsync():
     """
-    This will check if rsync exists and if it matches the version which was specified.
-    This is important, because different versions of RSYNC work slightly differently.
-    :param version:
-    :return bool:
+    This will run the final RSYNC.
     """
+    ...
+
+
+def findJobConfig(
+        job: str,
+        config: ConfigListDataClass
+) -> ConfigDataClass:
+    """
+    This will find the first job in the config that relates to the
+    specified job.
+    """
+    for c in config.jobs:
+        if c.job_name == job:
+            return c
+
+    raise ConfigJobNotFoundException('The job config was not found.')
+
+
+def parserSetup() -> argparse.Namespace:
+    """ Set up the argument parser for the application. """
+    parser = argparse.ArgumentParser('Backup Remote Files.')
+    parser.add_argument('-j', '--job',
+                        help='Specifies what config job to run.',
+                        type=str)
+    parser.add_argument('-s', '--show-available-jobs',
+                        help='Shows a list of jobs available in the config.',
+                        default=False,
+                        action='store_true')
+    parser.add_argument('-c', '--config',
+                        help='Specify a config file if you do not want '
+                             'to use the default config file (config.yaml).',
+                        default=DEFAULT_CONFIG, type=str)
+    parser.add_argument('-d', '--dry-run',
+                        help='Will run the rsync without making any '
+                             'changes to the source or destination.',
+                        default=False,
+                        action='store_true')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parserSetup()
+
     try:
-        # check if rsync exists
-        response_rsync = subprocess.run('rsync --version', shell=True, stdout=subprocess.PIPE,
-                                        stderr=subprocess.DEVNULL)
+        # Read the config file if available.
+        config_path = args.config if args.config else DEFAULT_CONFIG
+        config = readConfig(config_path)
 
-        if response_rsync.returncode != 0 and response_rsync.returncode != 1:
-            raise Exception("RSYNC not found on this OS.")
+        # If the user requested to see the available jobs, show a list.
+        if not args.job and args.show_available_jobs:
+            available_jobs = [a.job_name for a in config.jobs]
+            cprint(f'These jobs are available in your config file: '
+                   f'{available_jobs}', 'blue')
+            exit(60)
 
-        response_version = subprocess.check_output('rsync --version', shell=True)
-        version_re = re.findall(r"((version) (\d.\d.\d))", str(response_version))
+        if args.job:
+            # Check that the job exists in the config.
+            job_config = findJobConfig(args.job, config)
 
-        if version_re[0][2]:
-            if str(version_re[0][2]) == str(version):
-                return True
-            else:
-                raise Exception("Incorrect version detected: " + str(version_re))
-        else:
-            raise Exception("Could not determine version number")
+            # Check to see if the correct RSYNC is available.
+            rsyncCheck(RSYNC_VERSION)
 
-    except Exception as rsync_exception:
-        print("RSYNC ERROR: " + str(rsync_exception.args))
-        return False
+            # Build RSYNC command from job config if it exists
+            rsync_command = compileRsyncCommand(job_config)
 
-    return False
+            # Do RSYNC backup
+            start_time = time.time()
 
+            cprint(f'The job {args.job} took {(time.time() - start_time):.2f} '
+                   f'seconds to complete.', 'yellow')
 
-def print_help():
-    """
-    Helper method which will simply print the CMD help.
-    It will exit by default because it assumes that the program cannot continue.
-    :return:
-    """
-    print("./rsync_backup.py -h --help --config=[] --dry-run")
-    exit(3)
+    except Exception as e:
+        cprint(str(e), 'red')
 
 
-if __name__ == "__main__":
-
-    # Set the default Program config
-    PROGRAM_CONFIG = "config.yml"
-    RSYNC_DRY_RUN = False
-
-    try:
-        # Get program arguments
-        opts, args = getopt.getopt(sys.argv[1:], "h", ["help", "config=", "dry-run"])
-    except getopt.GetoptError as e:
-        print("Something went wrong : " + str(e.args))
-        print_help()
-        exit(2)
-
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            print_help()
-        elif opt in "--config":
-            print("CONFIG: {})".format(arg))
-            PROGRAM_CONFIG = arg
-        elif opt in "--dry-run":
-            print(">>> A DRY RUN")
-            RSYNC_DRY_RUN = True
-
-    # Load the PROGRAM YAML config
-    try:
-        config = yaml.safe_load(open(PROGRAM_CONFIG))
-    except Exception as configExteption:
-        print(configExteption.args)
-        exit(4)
-
-    # Set global configuration
-    BACKUP_NAME = config["backup_name"]
-    BACKUP_DESCRIPTION = config["backup_description"]
-    REMOTE_HOST = config["remote"]["host"]
-    REMOTE_USER = config["remote"]["user"]
-    SRC_PATH_LIST = config["local"]["path_list"]
-    DST_DRIVE_NAME = config["local"]["drive_name"]
-    DST_DRIVE_PATH = config["local"]["drive_path"]
-    DST_DRIVE_CHECK = config["local"]["drive_check"]
-    RSYNC_OPTIONS = config["rsync"]["options"]
-    RSYNC_VERSION = config["rsync"]["version"]
-    RSYNC_CHECK = config["rsync"]["check"]
-
-    # Print the program start stuff
-    start_time = time.time()
-    print(">>> START : {name} : {description}".format(name=BACKUP_NAME, description=BACKUP_DESCRIPTION))
-
-    # Check that RSYNC Requirement is met for this OS.
-    if RSYNC_CHECK:
-        rsync_check(RSYNC_VERSION)
-
-    # Check that the remote host is UP
-    if ping_host(REMOTE_HOST) != 0:
-        print("Remote host cannot be found on the network.")
-        exit(4)
-    if not host_socket_test(REMOTE_HOST, 22):
-        print("Remote host SSH port not available.")
-        exit(4)
-
-    # Check that the destination path exists
-    if DST_DRIVE_CHECK:
-        if not path_check(DST_DRIVE_PATH):
-            print(">>> Destination path does not exist. This could mean that the drive is not mounted correctly or at "
-                  "the destination specified in the config.")
-            exit(4)
-
-    # If everything has been checked, the RSYNC command can be generated and run.
-    rsync_cmd = generate_rsync_cmd(
-        REMOTE_USER,
-        REMOTE_HOST,
-        RSYNC_OPTIONS,
-        SRC_PATH_LIST,
-        DST_DRIVE_PATH,
-        RSYNC_DRY_RUN
-    )
-
-    rsync_response = subprocess.run(rsync_cmd, shell=True, stderr=subprocess.DEVNULL)
-
-    if rsync_response.returncode not in (0,1,23):
-        print("RSYNC could not be completed successfully.")
-
-    end_time = time.time() - start_time
-    print(">>> COMPLETE - {time:.2f} (Seconds)".format(time=end_time))
-
+if __name__ == '__main__':
+    main()
