fc8e2a8ed1e28784ccc7251625bec8355596f4ce
Gideon <gideon@mortolio.com>
2022-09-21 10:04:12 +0200

    Milestone commit for backup.

--- src/backup.py
+++ src/backup.py
@@ -131,16 +131,16 @@ def compileRsyncCommand(config: ConfigDataClass) -> str:
     command = f'rsync ' \
               f'{config.rsync_options} ' \
               f'{config.remote_user}@{config.remote_host}:' \
-              f'{" ".join(config.remote_paths)} {config.local_path}'
+              f'\'{",".join(config.remote_paths)}\' {config.local_path}'
 
     return command
 
 
 def validateRemotePath(path: str) -> None:
     """ Remote paths should have a trailing slash. """
-    if not path.endswith('/'):
-        raise PathValidationException(f'{path} should have a trailing slash '
-                                      f'in the job config.')
+    # if path.endswith('/'):
+    #     raise PathValidationException(f'{path} should have a trailing slash '
+    #                                   f'in the job config.')
 
 
 def validateLocalPath(path: str) -> None:
@@ -150,11 +150,19 @@ def validateLocalPath(path: str) -> None:
                                       f'a trailing slash in the job config.')
 
 
-def rsync():
+def rsync(command: str):
     """
     This will run the final RSYNC.
     """
-    ...
+    colored('blue')
+    response = subprocess.run(
+        command,
+        capture_output=True,
+        shell=True,
+        text=True
+    )
+    colored('white')
+    return response.returncode
 
 
 def findJobConfig(
